ef436defe55ffbdbf949de6408d15a88619b13d0
mostafizur-rahman <dev.mostafizurrahman@gmail.com>
2024-12-25 12:07:58 +0600

    create a specific api for getting specific id for update

--- index.js
+++ index.js
@@ -41,6 +41,14 @@ async function run() {
         const cursor= userCollection.find()
         const result = await cursor.toArray()
         res.send(result)
+    });
+
+    app.get("/users/:id", async(req,res)=>{
+        const id =req.params.id;
+        const query = {_id: new ObjectId(id)}
+        const user = await userCollection.findOne(query)
+        res.send(user)
+        
     })
 
     app.post("/users", async(req,res)=>{
